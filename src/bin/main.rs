469d39c60a162fc8ab922e3656a6454f41e3169e
Sayantan Talukder <toddwannacode@gmail.com>
2017-07-28 12:32:28 +0530

    Added threadpool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,6 @@
+extern crate server;
+
+use server::ThreadPool;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
@@ -9,7 +12,15 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
     let pool = ThreadPool::new(4);
 
+    let mut counter = 0;
+
     for stream in listener.incoming() {
+        if counter==2 {
+            println!("Shutting down!");
+            break;
+        }
+        counter += 1;
+
         let stream = stream.unwrap();
         pool.execute(|| {
             handle_connection(stream)
